28585b8da74de2790ac32bdc993d7e9a355554c9
lam999666333 <lam999666333@gmail.com>
2025-05-13 09:45:06 +0800

    perf: nav

--- resources/js/Pages/Test/Components/Test9Nav.vue
+++ resources/js/Pages/Test/Components/Test9Nav.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="container mx-auto flex justify-between items-center px-4 py-3">
     <ul class="flex gap-6">
-      <li><a href="#" class="hover:text-blue-500">首頁</a></li>
-      <li><a href="#" class="hover:text-blue-500">產品</a></li>
-      <li><a href="#" class="hover:text-blue-500">關於</a></li>
+      <li><a href="javascript:void(0)" class="hover:text-blue-500">首頁</a></li>
+      <li><a href="javascript:void(0)" class="hover:text-blue-500">產品</a></li>
+      <li><a href="javascript:void(0)" class="hover:text-blue-500">關於</a></li>
+      <li>
+        <a href="javascript:void(0)" class="hover:text-blue-500">
+          <MagnifyingGlassIcon class="size-5 inline-block" />
+        </a>
+      </li>
     </ul>
   </div>
 </template>
+
+<script setup lang="ts">
+import { MagnifyingGlassIcon } from '@heroicons/vue/24/solid'
+</script>
